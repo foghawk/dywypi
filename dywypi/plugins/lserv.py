a13455e39741fe53b95523e5941e968a377d6dd9
foghawk <scimitarfawkes@gmail.com>
2014-04-28 01:55:05 -0500

    added filetype filter

--- dywypi/plugins/lserv.py
+++ dywypi/plugins/lserv.py
@@ -9,6 +9,14 @@ plugin = Plugin('lserv', '@')
 logger = logging.getLogger(__name__)
 
 PATH = u"INSERT_TEST_PATH_HERE" #make sure this takes unicode
+TYPES = ["cbr", "cbz", "chm", "djvu", "docx?", "epub",
+	"fb2", "html?", "lit", "lrf", "mobi", "odt",
+	"pdb", "pdf", "prc", "rar", "rtf", "txt", "zip"]
+
+ts = '^[^\.].*('
+for t in TYPES:
+	ts = ts + t + '|'
+tf = re.compile(ts[:-1] + ')$')
 
 def safe_add(d, k, v, msg):
 	if k not in d:
@@ -20,19 +28,17 @@ fs = {}
 if os.path.isdir(PATH):
 	for rootpath, dirs, files in os.walk(os.path.abspath(PATH)):
 		for name in files:
-			filename = os.path.basename(name)
-			if filename[0] != '.': #fold this into filetype filter later
-				file_path = os.path.join(rootpath, name)
-				safe_add(fs, filename, file_path,
+			if tf.match(name):
+				safe_add(fs, name, os.path.join(rootpath, name),
 					'"{0}" collides with file "{1}" and will not be served.')
-			#progress indicator? filetype filter?
+			#progress indicator?
 			#TODO write to file--better caching, allows download of list (required). reserve filename?
 			#can i load from dir, then start serving while writing? should be able to, duh async
 			#TODO one bot serving multiple lists
 elif os.path.isfile(PATH):
 	l = open(PATH)
 	for line in l:
-		if os.path.isfile(line):
+		if os.path.isfile(line) and tf.match(os.path.basename(line)):
 			safe_add(fs, os.path.basename(line), line,
 				'"{0}" collides with file "{1}" and will not be served.')
 		else:
