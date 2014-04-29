7cb162a088d6774ffe446f071066991313fe464c
foghawk <scimitarfawkes@gmail.com>
2014-04-29 13:34:06 -0500

    formatting & file modes

--- dywypi/plugins/ctcp.py
+++ dywypi/plugins/ctcp.py
@@ -15,31 +15,31 @@ logger = logging.getLogger(__name__)
 
 @plugin.command('ctcp-VERSION')
 def version(event):
-	yield from event.reply("\x01VERSION dywypi {0}\x01".format(pkg_resources.get_distribution("dywypi").version), no_respond=True)
+    yield from event.reply("\x01VERSION dywypi {0}\x01".format(pkg_resources.get_distribution("dywypi").version), no_respond=True)
 
 @plugin.command('ctcp-PING')
 def ping(event):
-	yield from event.reply("\x01PING {0}\x01".format(event.message[10:]), no_respond=True)
+    yield from event.reply("\x01PING {0}\x01".format(event.message[10:]), no_respond=True)
 
 @plugin.command('ctcp-TIME')
 def time(event):
-	yield from event.reply("\x01TIME {0}\x01".format(datetime.datetime.now().isoformat()), no_respond=True)
+    yield from event.reply("\x01TIME {0}\x01".format(datetime.datetime.now().isoformat()), no_respond=True)
 
 @plugin.command('ctcp-DCC')
 def dcc(event):
-	if event.args[0] == 'CHAT':
-		std_ip = str(ipaddress.ip_address(int(event.args[2])))
-		net = Network(std_ip)
-		net.add_server(std_ip, int(event.args[3]))
-		dcc_client = DCCClient(event.loop, net)
-		try:
-			yield from dcc_client.connect()
-			yield from dcc_client.send_message("hello!")
-		except TimeoutError:
-			yield from event.reply("Sorry, it looks like I can't open a direct chat connection. This could be because of settings on your IRC client, firewall, router, and/or proxy.")
+    if event.args[0] == 'CHAT':
+        std_ip = str(ipaddress.ip_address(int(event.args[2])))
+        net = Network(std_ip)
+        net.add_server(std_ip, int(event.args[3]))
+        dcc_client = DCCClient(event.loop, net)
+        try:
+            yield from dcc_client.connect()
+            yield from dcc_client.send_message("hello!")
+        except TimeoutError:
+            yield from event.reply("Sorry, it looks like I can't open a direct chat connection. This could be because of settings on your IRC client, firewall, router, and/or proxy.")
 
 @plugin.on(DirectMessage)
 def dcc_echo(event):
-	logger.debug("got a direct message!") #no
-	logger.debug("it's '" +event.message+ "'")
-	yield from event.client.say(event.message)
+    logger.debug("got a direct message!") #no
+    logger.debug("it's '" +event.message+ "'")
+    yield from event.client.say(event.message)
--- dywypi/plugins/lserv.py
+++ dywypi/plugins/lserv.py
@@ -10,70 +10,70 @@ logger = logging.getLogger(__name__)
 
 PATH = u"INSERT_TEST_PATH_HERE" #make sure this takes unicode
 TYPES = ["cbr", "cbz", "chm", "djvu", "docx?", "epub",
-	"fb2", "html?", "lit", "lrf", "mobi", "odt",
-	"pdb", "pdf", "prc", "rar", "rtf", "txt", "zip"]
+    "fb2", "html?", "lit", "lrf", "mobi", "odt",
+    "pdb", "pdf", "prc", "rar", "rtf", "txt", "zip"]
 
 ts = '^[^\.].*('
 for t in TYPES:
-	ts = ts + t + '|'
+    ts = ts + t + '|'
 tf = re.compile(ts[:-1] + ')$')
 
 def safe_add(d, k, v, msg):
-	if k not in d:
-		d[k] = v
-	else:
-		logger.warning(msg.format(v, d[k]))
+    if k not in d:
+        d[k] = v
+    else:
+        logger.warning(msg.format(v, d[k]))
 
 fs = {}
 if os.path.isdir(PATH):
-	for rootpath, dirs, files in os.walk(os.path.abspath(PATH)):
-		for name in files:
-			if tf.match(name):
-				safe_add(fs, name, os.path.join(rootpath, name),
-					'"{0}" collides with file "{1}" and will not be served.')
-			#progress indicator?
-			#TODO write to file--better caching, allows download of list (required). reserve filename?
-			#can i load from dir, then start serving while writing? should be able to, duh async
-			#TODO one bot serving multiple lists
+    for rootpath, dirs, files in os.walk(os.path.abspath(PATH)):
+        for name in files:
+            if tf.match(name):
+                safe_add(fs, name, os.path.join(rootpath, name),
+                    '"{0}" collides with file "{1}" and will not be served.')
+            #progress indicator?
+            #TODO write to file--better caching, allows download of list (required). reserve filename?
+            #can i load from dir, then start serving while writing? should be able to, duh async
+            #TODO one bot serving multiple lists
 elif os.path.isfile(PATH):
-	l = open(PATH)
-	for line in l:
-		if os.path.isfile(line) and tf.match(os.path.basename(line)):
-			safe_add(fs, os.path.basename(line), line,
-				'"{0}" collides with file "{1}" and will not be served.')
-		else:
-			logger.warning('"{0}" is not a valid file and will not be served.'.format(line))
-			#TODO smarter error checking (don't log a million warnings if one external drive is missing)
+    l = open(PATH)
+    for line in l:
+        if os.path.isfile(line) and tf.match(os.path.basename(line)):
+            safe_add(fs, os.path.basename(line), line,
+                '"{0}" collides with file "{1}" and will not be served.')
+        else:
+            logger.warning('"{0}" is not a valid file and will not be served.'.format(line))
+            #TODO smarter error checking (don't log a million warnings if one external drive is missing)
 else:
-	logger.error('List path not found; nothing will be served!')
+    logger.error('List path not found; nothing will be served!')
 
 def and_search(terms, string):
-	for term in terms:
-		if not re.search(term, string, re.I):
-			return False
-	return True
+    for term in terms:
+        if not re.search(term, string, re.I):
+            return False
+    return True
 
 def prettysize(b):
-	for u in ['bytes', 'KB', 'MB', 'GB', 'TB']:
-		if b < 1024.0:
-			return '{0:.2f} {1}'.format(b, u)
-		b /= 1024.0
-	return '{0:.2f} TB'.format(b)
+    for u in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+        if b < 1024.0:
+            return '{0:.2f} {1}'.format(b, u)
+        b /= 1024.0
+    return '{0:.2f} TB'.format(b)
 
 @plugin.command('find')
 def find(event):
-	results = {f: fs[f] for f in fs if and_search(event.args, f)}
-	if len(results) == 0 and not event.channel:
-		yield from event.reply("Sorry, nothing found for '{0}'.".format(event.message[6:]))
-	for r in results:
-		yield from event.reply("{0} ({1})".format(r, prettysize(os.path.getsize(os.path.join(r, results[r])))), private=True)
+    results = {f: fs[f] for f in fs if and_search(event.args, f)}
+    if len(results) == 0 and not event.channel:
+        yield from event.reply("Sorry, nothing found for '{0}'.".format(event.message[6:]))
+    for r in results:
+        yield from event.reply("{0} ({1})".format(r, prettysize(os.path.getsize(os.path.join(r, results[r])))), private=True)
 
 @plugin.on(PublicMessage)
 def send(event):
-	if event.message == '@'+event.client.nick: #can't implement as command; no access to nick w/o event :\
-		pass #send list
-	elif event.message.startswith('!'+event.client.nick):
-		request = fs[event.message[len(event.client.nick)+2:]]
-		logger.debug('Ready to serve file at '+request)
-		#send file. require ctcp/dcc plugin; that works, right?
+    if event.message == '@'+event.client.nick: #can't implement as command; no access to nick w/o event :\
+        pass #send list
+    elif event.message.startswith('!'+event.client.nick):
+        request = fs[event.message[len(event.client.nick)+2:]]
+        logger.debug('Ready to serve file at '+request)
+        #send file. require ctcp/dcc plugin; that works, right?
 
